48c8942753c87c9259bcf60869e2e95263c877b0
ShingenTakeda <gustakeda@gmail.com>
2023-07-28 12:12:15 -0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+    import "../app.postcss"
+</script>
+
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  
+  <style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+    }
+  </style>
+
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/settings">Settings</a>
+</nav>
+
+<slot></slot>
